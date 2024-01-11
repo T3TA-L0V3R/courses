39e7ffedf8e959d3b59a2a681199ff793a93cfae
Aaron <agiles@roistom.com>
2024-01-11 14:00:35 -0600

    ...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,23 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Roles;
 use Illuminate\Http\Request;
 
-class RolesController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
     {
         //
     }
@@ -34,15 +25,7 @@ class RolesController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Roles $roles)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Roles $roles)
+    public function show(string $id)
     {
         //
     }
@@ -50,7 +33,7 @@ class RolesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Roles $roles)
+    public function update(Request $request, string $id)
     {
         //
     }
@@ -58,7 +41,7 @@ class RolesController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Roles $roles)
+    public function destroy(string $id)
     {
         //
     }
